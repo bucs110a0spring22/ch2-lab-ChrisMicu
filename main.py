b36836a59bbd3f293b353ae6b667d16bcab04af4
Christopher Micu <98533751+ChrisMicu@users.noreply.github.com>
2022-02-14 17:20:45 +0000

    Spoke to TA

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
 classes_per_week = 5
-print("Classes Per Week:" , classes_per_week)
+print(classes_per_week, type(classes_per_week))
 cost_per_class = (cost_per_week / classes_per_week)
-print("Cheap Cost Per Class:" , cost_per_class)
+print(cost_per_class , type(cost_per_class))
+
 
 #Part B
-list = ["Health" , "Time" , "Money" , "Power" , "Knowledge"]
-rando = random.choice(list)
-print(rando)
+liist = ["Health" , "Time" , "Wealth" , "Power" , "Knowledge"]
+rando = random.choice(liist)
+print("Random goal to achieve:" , rando)
